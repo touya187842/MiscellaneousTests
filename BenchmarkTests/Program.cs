60378085411c5d54c0184f3a13afbf9d4708fac1
touya187842 <touya187842@yahoo.co.jp>
2024-04-06 17:28:28 +0800

    add benchmark project

---
+++ BenchmarkTests/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace BenchmarkTests;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // BenchmarkRunner.Run<T>();
+    }
+}
